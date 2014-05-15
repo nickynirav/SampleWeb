599e409937a1700d6a2a0c220fa7f183b0d04982
Developer <Developer@test.com>
2014-05-14 20:55:35 -0700

    first commit

---
+++ src/main/java/com/picsauditing/action/WelcomeUserAction.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package com.picsauditing.action;
+
+/**
+ * @author developer
+ * 
+ */
+public class WelcomeUserAction {
+
+	private String username;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	// all struts logic here
+	public String execute() {
+
+		return "SUCCESS";
+
+	}
+
+}
